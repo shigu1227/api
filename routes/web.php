41605cbd42a60596f48cd5c3e7d8bf1390fe24be
shigu1227 <2436229636@qq.com>
2020-01-03 19:23:07 +0800

    admin

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,10 @@ Route::any('/test/pay','TestController@alipay');
 
 Route::get('/test/alipay/return','Alipay\PayController@aliReturn');
 Route::post('/test/alipay/notify','Alipay\PayController@notify');
+
+
+//jiekou
+Route::get('/api/test','Api\TestController@test');
+Route::post('/api/user/reg','Api\TestController@reg');          //用户注册
+Route::post('/api/user/login','Api\TestController@login');      //用户登录
+Route::get('/api/user/list','Api\TestController@userList');      //用户列表
