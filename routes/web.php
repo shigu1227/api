df53d24a0569409b9cb8c1637cb16792b327361a
shigu1227 <2436229636@qq.com>
2020-02-04 19:25:04 +0800

    XX

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    echo 123;die;
-});
 
 Route::get('/', function () {
     return view('welcome');
